f43ff53ea0d0272f03f79d11cc967280d81813e6
LiGuer <46878543+LiGuer@users.noreply.github.com>
2020-10-31 12:50:23 +0800

    Update go.h

--- src/go.h
+++ src/go.h
@@ -7,7 +7,7 @@
 #include<time.h>
 typedef signed char INT8S;
 typedef unsigned char INT8U;
-typedef INT8S CHESS;
+typedef INT8S STONE;
 /******************************************************************************
 *                    状态
 ******************************************************************************/
@@ -18,8 +18,9 @@ public:
 		free(map);
 	}
 	INT8S x, y;
-	CHESS* map = NULL;
+	STONE* map = NULL;
 	INT8S player = 0;
+	INT8S Jie[2] = { -1,-1 };
 };
 /******************************************************************************
 *                    蒙特卡洛树搜索 节点
@@ -38,8 +39,6 @@ struct MCTSNode {
 	MCTSNode* parent = NULL;
 	int childCur = 0;
 	bool isFullChildFlag = false;
-	bool isFullChild() {
-	}
 };
 /******************************************************************************
 *                    围棋 Go
@@ -49,32 +48,35 @@ public:
 	/*---------------- 基础参数 ----------------*/
 	const static INT8S MapSize = 9;									//棋盘尺寸
 	const static INT8S  Black = 1, White = -1;
-	CHESS* Map;														//棋盘
-	const int MctsBudget = 100000;
+	STONE* Map;														//棋盘
+	const int MctsBudget = 10000;
 	const INT8S NOTPOINT = 10;										//禁入点标记//眼点标记 = 禁入点标记+棋子标记
+	const INT8S x_step[4] = { 1,-1,0,0 }, y_step[4] = { 0,0,1,-1 };
 	/*---------------- 基础函数 ----------------*/
 	void init();													//初始化
 	void clearMap();												//清盘
-	bool setMap(INT8S x, INT8S y, CHESS chess);						//下子
+	bool setMap(INT8S x, INT8S y, STONE stone);						//下子
 	bool judgeOutMap(int x, int y);									//判断是否过界
+	void GoAI(STONE* map, INT8S player, INT8S& x0, INT8S& y0);
 	/*---------------- [MCTS]核心函数 ----------------*/
 	MCTSNode* MCTS(MCTSNode* node);									//[MCTS]蒙特卡洛树搜索
 	MCTSNode* Select(MCTSNode* node, bool isExploration);			//[1]选择最优节点
-	MCTSNode* Expand(MCTSNode* node);								//[2]拓展新节点
+	bool Expand(MCTSNode* node, MCTSNode** newnode);								//[2]拓展新节点
 	int Simulation(State* state0);									//[3]模拟,快速走子
 	void Backpropagation(MCTSNode* node, int reward);				//[4]回溯
 	double UCB(MCTSNode* node, double C);							//UCB公式
 	MCTSNode* TreePolicy(MCTSNode* node);							//
-	bool nextStateRand(CHESS* map, INT8S& x0, INT8S& y0, CHESS chess);//随机走子
+	bool nextStateRand(State* state);//随机走子
+	bool nextStateRand(MCTSNode* node, State* state);
 	/*---------------- 围棋规则函数 ----------------*/
-	bool downChess(CHESS* map, INT8S x0, INT8S y0, CHESS chess);	//落子//[RULE 1]:无气提子
-	void judgeNotPoint(CHESS* map, CHESS chess);					//[RULE 2]:非提禁入
-																	//[RULE 3]:劫停一手
-	int judgeWin(CHESS* map);										//[RULE 4]:局势判定(数子法)
-	void ComputerQi(CHESS* map, INT8U qi[], INT8U chBlockMap[]);	//棋块数气
-	void GoAI(CHESS* map, INT8S player, INT8S& x0, INT8S& y0);
+	bool downStone(STONE* map, INT8S x0, INT8S y0, STONE stone);	//[RULE 1]:无气提子 //落子
+	void ComputerQi(STONE* map, INT8U qi[], INT8U chBlockMap[]);	//棋块数气
+	void judgeNotPoint(STONE* map, STONE stone, INT8U qi[], INT8U chBlockMap[]);//[RULE 2]:非提禁入 //一方禁入点标记
+	bool judgeJie(STONE* map, STONE stone, INT8S x0, INT8S y0,INT8U qi[], INT8U chBlockMap[], INT8S Jie0[]);	//[RULE 3]:劫停一手//判定劫
+	void judgeEye(STONE* map, INT8U qi[], INT8U chBlockMap[]);
+	int judgeWin(STONE* map);										//[RULE 4]:局势判定(数子法)
 	/*---------------- 调试函数 ----------------*/
-	void showMap(CHESS* map);
+	void showMap(STONE* map);
 	void showTree(MCTSNode* root);
 };
 #endif // MAINWINDOW_H
