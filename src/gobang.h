63412e9780b80cb9bfd9422f76c8c61a131c7be8
LiGuer <2975395623@qq.com>
2021-05-02 15:12:06 +0800

    Update

--- src/gobang.h
+++ src/gobang.h
@@ -50,7 +50,7 @@ namespace GoBang_AI {
 		run(boardState, x, y);
 	}
 	void run(State& board, int& x, int& y) {
-		MiniMax<State> AI(evaluate, newStateFunc, judgeWin, 0);
+		MiniMax<State> AI(evaluate, newStateFunc, judgeWin);
 		int maxScore = AI.Policy(0, board, -0x7fffffff, 0x7fffffff);
 		x = AI.maxScoreState.pos / BOARD_SIZE;
 		y = AI.maxScoreState.pos % BOARD_SIZE;
