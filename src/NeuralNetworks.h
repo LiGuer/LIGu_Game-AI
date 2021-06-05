119c87ac3c1604e3ddde6a9b3885b06283ecc6f8
LiGuer <2975395623@qq.com>
2021-06-05 23:22:36 +0800

    Update

--- src/NeuralNetworks.h
+++ src/NeuralNetworks.h
@@ -199,12 +199,13 @@ public:
 		static Tensor<> delta; delta.eat(error);
 		error.alloc(preIn.dim).zero();
 		for (int i = 0; i < error.size(); i++)
-			for (int zi = 0; zi < kernel.dim[3]; zi++)
-				for (int k = 0; k < kernel.size(2); k++) {
+			for (int zo = 0; zo < kernel.dim[3]; zo++)
+				for (int k = kernel.size(2) - 1; k >= 0; k--) {
 					int xi = -padding + error.i2x(i) * stride + kernel.i2x(kernel.size(2) - 1 - k),
-						yi = -padding + error.i2y(i) * stride + kernel.i2y(kernel.size(2) - 1 - k);
+						yi = -padding + error.i2y(i) * stride + kernel.i2y(kernel.size(2) - 1 - k),
+						zi =            error.i2z(i);
 					if (xi < 0 || xi >= delta.dim[0] || yi < 0 || yi >= delta.dim[1]) continue;
-					error(i) += delta(xi, yi, zi) * kernel(kernel.size(2) - 1 - k + error.i2z(i) * kernel.size(2) + zi * kernel.size(3));
+					error(i) += delta(xi, yi, zo) * kernel(k + zi * kernel.size(2) + zo * kernel.size(3));
 				}
 		// ∂E / ∂k_l
 		static Tensor<> deltaKernel; deltaKernel.alloc(kernel.dim).zero();
@@ -253,6 +254,7 @@ public:
 class PoolLayer {
 public:
 	Tensor<> out;
+	Tensor<> MaxIndexMem;
 	int kernelSize, padding, stride, poolType = 0;
 	enum { A, M };
 	/*----------------[ init ]----------------*/
@@ -271,6 +273,7 @@ public:
 			(in.dim[1] - kernelSize + 2 * padding) / stride + 1,
 			 in.dim[2]
 		);
+		if (poolType == M) MaxIndexMem.alloc(out.dim);
 		for (int i = 0; i < out.size(); i++)
 			for (int k = 0; k < kernelSize * kernelSize; k++) {
 				int xt = -padding + out.i2x(i) * stride + k % kernelSize,
@@ -279,7 +282,7 @@ public:
 				if (xt < 0 || xt >= in.dim[0] || yt < 0 || yt >= in.dim[1]) continue;
 				switch (poolType) {
 				case A: out(i) += in(xt, yt, zt); break;
-				case M: out(i)  = in(xt, yt, zt) > out(i) ? in(xt, yt, zt) : out(i); break;
+				case M: out(i)  = in(xt, yt, zt) > out(i) ? MaxIndexMem(i) = in.xyz2i(xt, yt, zt), in(xt, yt, zt) : out(i); break;
 				}
 			}
 		if (poolType == A) out *= 1.0 / (kernelSize * kernelSize);
@@ -299,18 +302,8 @@ public:
 					error(i) += delta(xt, yt, zt) / (kernelSize * kernelSize);
 				}
 		}
-		if (poolType == M) {
-			for (int i = 0; i < delta.size(); i++) {
-				double maxn = -DBL_MAX; int maxIndex = 0;
-				for (int k = 0; k < kernelSize * kernelSize; k++) {
-					int xt = -padding + delta.i2x(i) * stride + k % kernelSize,
-						yt = -padding + delta.i2y(i) * stride + k / kernelSize,
-						zt = delta.i2z(i);
-					if (xt < 0 || xt >= preIn.dim[0] || yt < 0 || yt >= preIn.dim[1]) continue;
-					if (preIn(xt, yt, zt) > maxn) maxn = preIn(xt, yt, zt), maxIndex = xt * yt * zt;
-				} error(maxIndex) += delta(i);
-			}
-		} return error;
+		if (poolType == M) for (int i = 0; i < delta.size(); i++) error(MaxIndexMem(i)) += delta(i);
+		return error;
 	}
 };
 /*********************************************************************************
@@ -665,8 +658,8 @@ public:
 class LeNet {
 public:
 	ConvLayer 
-		Conv_1{  1, 16, 5, 2, 1, sigmoid, sigmoidD },
-		Conv_2{ 16, 32, 5, 2, 1, sigmoid, sigmoidD };
+		Conv_1{  1, 16, 5, 2, 1, relu, reluD },
+		Conv_2{ 16, 32, 5, 2, 1, relu, reluD };
 	PoolLayer 
 		MaxPool_1{ 2, 0, 2, MaxPool_1.M }, 
 		MaxPool_2{ 2, 0, 2, MaxPool_2.M };
