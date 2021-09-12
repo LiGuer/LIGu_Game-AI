d53099d262071eb410b874e93463246ccb8a32db
LiGuer <2975395623@qq.com>
2021-09-12 13:28:40 +0800

    Update

---
+++ src/BasicMachineLearning.h
@@ -0,0 +1,247 @@
+/*
+Copyright 2020,2021 LiGuer. All Rights Reserved.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+	http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+#ifndef BASIC_MACHINE_LEARNING_H
+#define BASIC_MACHINE_LEARNING_H
+#include "../../LiGu_AlgorithmLib/Mat.h"
+#include <float.h>
+#include <algorithm>
+#include <vector>
+namespace BasicMachineLearning {
+/*#############################################################################
+
+						降维
+
+##############################################################################*/
+/******************************************************************************
+[主成分分析]: Principal Components Analysis 
+	[输入/输出]:
+		输入: x 原矩阵		输出: y 降维后矩阵
+	[目标]:
+		数据降维，提取数据的主要特征分量, 满足：
+		(1) 最近重构性: 样本点到该超平面的距离都足够近。
+		(2) 最大可分性: 样本点在该超平面的投影尽可能分开。
+	[流程]: 
+		(1) 数据中心化, Σ \vec x_i = 0
+		(2) 计算协方差矩阵 C = X X^T
+		(3) 对协方差矩阵C 特征值分解
+		(4) 取最大的d'个特征值所对应的特征向量{w1,w2,...,wd'},投影矩阵 W = (w1,w2,...,wd')
+		(5) 样本点在超平面投影: y_i = W^T x_i
+	[原理]:
+		分别从目标(1, 2)可以推得同样的结果。
+		样本点到超空间投影: y = WT x, 尽可能分开即.方差最大:max Σ WT x xT W
+		协方差矩阵:
+			D = 1/m Y Y^T = 1/m (PX) (PX)^T = 1/m P X X^T P^T = 1/m P C P^T
+		协方差矩阵对角化
+		=>	min_W	tr( W^T x x^T W )
+			st.		W^T W = I
+		Lagrange函数 L(,λ) = W^T x x^T W + λ( W^T W - I )
+		Lagrange对偶 G(λ) = inf L(,λ) = inf (W^T x x^T W + λ( W^T W - I ))
+		L(,λ)求导, 当导数为0时, 取得极值
+		=>	X X^T ω_i = λ_i ω_i,
+		即.对协方差X XT, 特征值求解
+		取特征值最大的yDim个特征向量, 即目标投影矩阵W
+*******************************************************************************/
+void PCA(Mat<>& x, Mat<>& y, int yDim) {
+	//[1]
+	Mat<> mean;
+	mean.mul(1.0 / x.cols, x.sum(mean, 1));		//得到均值
+	Mat<> x2(x);
+	for (int i = 0; i < x.rows; i++)
+		for (int j = 0; j < x.cols; j++)
+			x2(i, j) -= mean[i];
+	//[2]
+	Mat<> Cov;
+	Cov.mul(x2, x2.transpose(Cov));		//X*XT
+	//[3] 
+	Mat<> eigVec, eigValue;
+	Cov.eig(1e-5, eigVec, eigValue);
+	//[4] // sort
+	Mat<> eigValueTemp(1, eigValue.cols), W(eigVec.rows, yDim);
+	for (int i = 0; i < eigValue.cols; i++)eigValueTemp[i] = eigValue(i, i);
+	std::sort(eigValueTemp.data, eigValueTemp.data + eigValueTemp.cols);
+	// W
+	for (int i = 0; i < yDim; i++) {
+		double value = eigValueTemp[eigValueTemp.cols - 1 - i];
+		for (int j = 0; j < eigValue.cols; j++) {
+			if (eigValue(j, j) == value) {
+				for (int k = 0; k < eigVec.rows; k++) W(k, i) = eigVec(k, j);
+				eigValue(j, j) = -DBL_MAX;
+				break;
+			}
+		}
+	}
+	//[5]
+	y.mul(W.transpose(y), x);
+}
+
+/*#############################################################################
+
+						聚类
+
+##############################################################################*/
+/******************************************************************************
+[K-Mean 聚类]
+	[目标]:
+		聚类. 对N维分布的数据点，可以将其聚类在 K 个关键簇内. 
+	[步骤]:
+		(1) 随机选择 K 个簇心点 Center
+		(2) 迭代开始
+			(3) 归零 Cluster , Cluster: 簇,记录ith簇内的数据指针。
+			(4) 计算每个xi到簇心μj的距离
+				(5) 选择距离最小的簇心, 将该点加入其簇内
+			(6) 对每个簇,计算其质心 Center'
+			(7) Center≠Center' , 则更正Center为 Center'
+			(8) 迭代重新开始
+		(9) 一轮无更正时，迭代结束
+*******************************************************************************/
+void K_Mean(Mat<>& x, int K, Mat<>& Center, std::vector<int>* Cluster, int TimeMax = 0x7FFFFFFF) {
+	int Dim = x.rows, N = x.cols;
+	Center.zero(Dim, K);
+	//[1] 随机选择 K 个簇心点 
+	for (int i = 0; i < K; i++) {
+		int index = rand() % N;
+		for (int dim = 0; dim < Dim; dim++) Center(dim, i) = x(dim, index);
+	}
+	//[2]
+	for (int times = 0; times < TimeMax; times++) {
+		//[3]
+		for (int k = 0; k < K; k++) Cluster[k].clear();
+		//[4] 计算每个xi到Center_j的距离
+		for (int i = 0; i < N; i++) {
+			Mat<> d(1, K);
+			for (int k = 0; k < K; k++)
+				for (int dim = 0; dim < Dim; dim++)
+					d[k] += pow(x(dim, i) - Center(dim, k), 2);
+			//[5]
+			int index; d.min(index);
+			Cluster[index].push_back(i);
+		}
+		//[6] 对每个簇,计算其质心 Center'
+		Mat<> CenterTmp(Dim, K);
+		for (int k = 0; k < K; k++) {
+			for (int dim = 0; dim < Dim; dim++) {
+				for (int j = 0; j < Cluster[k].size(); j++)
+					CenterTmp(dim, k) += x(dim, Cluster[k][j]);
+				CenterTmp(dim, k) /= Cluster[k].size();
+			}
+		}
+		//[7] 更正簇心//[9]
+		if (CenterTmp == Center) return;						
+		Center.swap(CenterTmp);
+	}
+}
+
+/*################################################################################
+
+						插值
+
+#################################################################################*/
+/******************************************************************************
+[Lagrange插值]
+	[原理]:
+		f(x) = Σ_(i=1)^n  y_i · f_i(x)
+		f_i(x) = Π_(j=1,i≠j)^n  (x - x_j) / (x_i - x_j)
+		第N点y = 基函数1×第1点y + 基函数2×第2点y + 基函数3×第3点y
+		基函数状态2 = (输入X-第1点x)(输入X-第3点x) / (第2点x-第1点x)(第2点x-第3点x)
+******************************************************************************/
+double LagrangeInterpolation(double x, double* x0, double* y0, int n) {
+	double y = 0, t;
+	for (int i = 0; i < n; i++) {
+		t = y0[i];
+		for (int j = 0; j < n; j++)
+			if (i != j)
+				t *= (x - x0[j]) / (x0[i] - x0[j]);
+		y += t;
+	}
+	return y;
+}
+/******************************************************************************
+[样条插值]
+	[算法]: 过求解三弯矩方程组得出曲线函数组的过程
+******************************************************************************/
+void CubicSpline(double x, double y) {
+
+}
+/*********************************************************************************
+[反距离加权插值]: Inverse Distance Weighted
+	[目的]:
+		空间插值
+	[原理]:
+		插值点与样本点间的距离为权重进行加权平均，离插值点越近的样本点赋予权重越大。
+		f(x) = Σ w_i(x) f(x_i)
+		w_i(x) = (1/d_i(x)^α) / (Σ 1/d_i(x)^α)
+		d_i(x) = ||x - x_i||₂
+*********************************************************************************/
+Mat<>& InvDisWeight(Mat<>& data, double xs, double xe, double ys, double ye, double dx, double dy, Mat<>& z, int alpha = 2) {
+	int xn = (xe - xs) / dx + 1,
+		yn = (ye - ys) / dy + 1;
+	z.zero(xn, yn);
+	for (int r = 0; r < xn; r++) {
+		for (int c = 0; c < yn; c++) {
+			double x = xs + r * dx, y = ys + c * dy;
+			double d0 = 0;
+			for (int i = 0; i < data.cols; i++) {
+				d0 += pow((pow(x - data(0, i), 2) + pow(y - data(1, i), 2)), -alpha);
+			}
+			for (int i = 0; i < data.cols; i++) {
+				double d = pow((pow(x - data(0, i), 2) + pow(y - data(1, i), 2)), -alpha);
+				z(r, c) += (d / d0) * data(2, i);
+			}
+		}
+	}
+	return z;
+}
+/*********************************************************************************
+[Kriging插值]:
+	[目的]:
+		空间插值
+	[原理]:
+		f(x) = Σ w_i(x) f(x_i)
+		权重系数是能够满足点(xo,yo)处估计值与真实值之差最小的一套最优系数
+
+*********************************************************************************/
+void Kriging() {
+
+}
+
+
+
+
+/******************************************************************************
+*				Mahalanobis Distance
+* Mahalanobis Distance
+*******************************************************************************/
+void MahalanobisDist(Mat<>& x, Mat<>& mahalanobisDistance) {
+	mahalanobisDistance.alloc(x.size());
+	// mean, diff, cov
+	Mat<> mean, diff, covMat, tmp;
+	mean.mul(1.0 / x.size(), x.sum(mean, 1));
+	covMat.mul(x, x.transpose(covMat));
+	// mahalanobis distance
+	covMat.inv(covMat);
+	Mat<> xt;
+	for (int i = 0; i < x.size(); i++) {
+		diff.sub(x.getCol(i, xt), mean);
+		tmp.mul(tmp.mul(diff.transpose(tmp), covMat), diff);
+		mahalanobisDistance[i] = tmp[0];
+	}
+}
+double MahalanobisDist(Mat<>& x1, Mat<>& x2, Mat<>& covMat) {
+	double ans;
+	Mat<> diff, _covMat, tmp;
+	diff.sub(x1, x2);
+	covMat.inv(_covMat);
+	return ((diff.transpose(tmp) *= _covMat) *= diff)[0];
+}
+}
+#endif
---
+++ src/ReinforcementLearning.c
@@ -0,0 +1,153 @@
+﻿/*
+Copyright 2020,2021 LiGuer. All Rights Reserved.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+	http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+#include "Mat.h"
+#include <float.h>
+#include <algorithm>
+#include <vector>
+/******************************************************************************
+*								QLearning
+*	[定义]:Q(s,a) = (1 + lr)·Q(s,a) + lr·( R + g·max Q(s',:) )
+			s: state	a: action	R: reward	lr: learning rate	g: forget factor
+*	[原理]:
+		选择动作: ε-greedy方法:
+			每个状态以ε概率随机选取动作，1-ε概率选择当前最优解
+		眼前利益R: 反馈值.
+		记忆中的利益 max Q(s',:): 记忆里，新位置s'能给出的最大效用值.
+		forget factor越大，越重视以往经验，越小，则只重视眼前利益R.
+*	[流程]:
+		[1] Init Q table arbitrarily
+		[2] Repeat (for each episode), until s is terminal
+			[3] Choose a from s using policy derived from Q (eg. ε-greedy)
+			[4] Take action a, observe r s'
+			[5] Q(s,a) = (1 + lr)·Q(s,a) + lr·( R + g·max Q(s',:) )
+				s = s'
+*	[Ps]:
+		可以逐渐降低随机选取动作的概率ε，一开始随机率可达100%
+			然后随训练次数的深入，应当逐渐降低随机概率。
+*******************************************************************************/
+class QLearning {
+public:
+	/*---------------- 核心数据 ----------------*/
+	Mat<> QTable;
+	double learnRate = 0.6,
+		forgetRate = 0.8,
+		greedy = 0.9;
+	/*---------------- 初始化 ----------------*/
+	QLearning() { ; }
+	QLearning(int _stateNum, int _actionNum) { init(_stateNum, _actionNum); }
+	void init(int _stateNum, int _actionNum) { QTable.zero(_stateNum, _actionNum); }
+	/*---------------- 选择行为 ----------------
+	*	[1] 探索模式(随机)	[2] 利用模式(最优)
+	-------------------------------------------*/
+	int operator()(int state) { return forward(state); }
+	int forward(int state) {
+		int action = 0;
+		double maxQ = -DBL_MAX,
+			minQ = DBL_MAX;
+		for (int i = 0; i < QTable.cols; i++) {
+			maxQ = QTable(state, i) > maxQ ? action = i, QTable(state, i) : maxQ;
+			minQ = QTable(state, i) < minQ ? QTable(state, i) : minQ;
+		}
+		//[1]
+		if ((minQ == 0 && maxQ == 0) || rand() / double(RAND_MAX) < greedy)
+			return rand() % QTable.cols;
+		//[2]
+		return action;
+	}
+	/*---------------- 反馈学习 ----------------*/
+	void backward(int state, int action, double reward, int& preState) {
+		double maxQ = -DBL_MAX;
+		for (int i = 0; i < QTable.cols; i++)
+			maxQ = QTable(state, i) > maxQ ? QTable(state, i) : maxQ;
+		QTable(preState, action) = (1 - learnRate) * QTable(preState, action) + learnRate * (reward + forgetRate * maxQ);
+		preState = state;
+	}
+};
+/******************************************************************************
+*                    Deep Q Network
+*	[思想]: Q-Learning + Neural Network
+		[Loss Function]: 近似值和真实值的均方差
+*	[流程]:
+	[1] Initialize replay memory D to capacity N
+		Initialize action-value function Q with random weights θ
+		Initialize target action-value function Q with weights θ- = θ
+	[2] for episode 1,M do
+		[] for t= 1,T do
+			[] Initialize sequence 81 = {x1 } and preprocessed sequenceΦ1 =Φ(81)
+			[] With probability ε select a random action at
+			[] otherwise select at = arg maxa Q(Φ(8t),a;0)
+			[] Execute action at in the emulator and observe reward rt and image rt+1
+			[] Setst+1 = 8t,at,2t+1 and preprocess中+1 =Φ(8+1)
+			[] Store experience (φ,at,rt,φ++1)in D
+			[] Sample random minibatch of experiences (Φ, aj,rj,Φj+1) from D
+			[] Setyj=	{ rj				if episode terminates at stepj+1
+					(rj + r maxa' Q(φj+1,a';0-) otherwise
+			[] Perform a gradient descent step on (9j - Q(Φj, aj; 0))2 with respect to the weights θ
+			[] Every C steps reset Q=Q
+******************************************************************************/
+class DeepQNetwork {
+public:
+	double learnRate = 0.6,
+		forgetRate = 0.8,
+		greedy = 0.9;
+	int actionNum = 0,
+		stateNum = 0;
+	/*---------------- 选择行为 ----------------*/
+	int operator()(int state) { return forward(state); }
+	int forward(int state) {
+		int action = 0;
+		if (rand() / double(RAND_MAX) < greedy)
+			return action = rand() % actionNum;
+		Mat<> ans;// = evalNet(state);
+		ans.max(action);
+		return action;
+	}
+	/*---------------- 反馈学习 ----------------*/
+	void backward(int state, int action, double reward, int& preState) {
+	}
+};
+/******************************************************************************
+*                    Support Vector Machines 支持向量机
+*	[定义]: 特征空间上, [间隔最大]的线性分类器.
+		[超平面]: y = w x + b
+		[间隔]: 样本点, 到超平面的距离.
+				γi = 1/||w||·|w x + b|	(点到面距离公式)
+		[样本点]: {(xi,yi)} i=1toN    xi∈R_n 实向量    yi∈{-1,1}
+*	[目标]: 找到目标超平面, 使得[所有样本点间隔最小值γmin = min γi]最大.
+			max_wb( min_i 1/||w||·|w xi + b| )
+			st.  w x + b > 0 , yi = +1  and  w x + b < 0 , yi = -1  (分类)
+		[推导]:
+		=>	|w xi + b| = yi(w xi + b)		//去绝对值
+		=>	max_wb( 1/||w||·min_i yi(w xi + b) )    st. yi(w x + b) > 0
+		转化为:
+			min_wb 1/2·||w||²    st. yi (W x + b) ≥ 1
+			凸二次规划问题, 用拉格朗日乘子法, 得其对偶问题.
+*	[结论]:
+		min 1/2·Σi Σj αi αj yi yj K(xi,xj) - Σi αi	//线性时 K(xi,xj)即内积
+		st. Σi αi yi = 0    0≤αi≤C
+*	[Kernal Trick]: 升到高维, 实现非线性分类
+		[Classical Kernal Function]:
+		* 高斯核函数: K(x,z) = exp( -||x - z||² / 2σ² )
+*	[二次规划优化问题]:
+		[算法]: Sequential Minimal Optimization 算法
+		[思路]: 若所有变量解都满足此最优化问题的KKT条件，则得到最优化问题解
+		[Karush Kuhn Tucker条件]: 非线性规划最佳解的必要条件
+*	[流程]:
+		[1] 选择惩罚参数 C > 0, 构造并求解凸二次规划问题, 得到最优解α
+		[2] w = Σi αi yi xi    b = yj - Σαi yi (xi·xj)
+		[3] 得到分离超平面 w x + b = 0
+			分类决策函数: f(x) = sign( w x + b )
+******************************************************************************/
+//void SupportVectorMachines(Mat<> X, Mat<int> Y) {
+
+//};
